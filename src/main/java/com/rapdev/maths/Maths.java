acff7e0350ef5f4bdc4fd975d1842ada588cce7f
Jon Giara <jgiara28@gmail.com>
2026-06-30 17:42:37 -0500

    Create Maths.java

---
+++ src/main/java/com/rapdev/maths/Maths.java
@@ -0,0 +1,45 @@
+package com.rapdev.maths;
+
+/**
+ * 
+ * A dummy Math class used for testing the Jenkins pipeline builds.
+ * 
+ */
+public class Maths 
+{
+    public int add(int a, int b) {
+        int c = a;
+        int d = b;
+        int result;
+        result = c + d;
+    	return a + b;
+    }
+    
+    public int subtract(int a, int b) {
+        int c = a;
+        int d = b;
+        int result;
+        result = c - d;
+    	return a - b;
+    }
+    
+    public int multiply(int a, int b) {
+        int c = a;
+        int d = b;
+        int result;
+        result = c * d;
+    	return a * b;
+    }
+    
+    public int divide(int a, int b) {
+        int c = a;
+        int d = b;
+        int result;
+        result = c / d;
+    	return a / b;
+    }
+    
+    public String doStuff(int a) {
+        return "hello!";
+    }
+}
